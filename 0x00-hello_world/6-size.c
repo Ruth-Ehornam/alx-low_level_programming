8e683cf4ed9b71bf4cd608a4f91d2d678aca2806
Ruth-Ehonram <ehornam7@gmail.com>
2023-02-16 12:34:50 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,13 @@
  * main - Entry
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("Size of a char: %d btye(s)\n", sizeof(char));
-	printf("Size of an int: %d btye(s)\n", sizeof(int));
-	printf("Size of a long int: %d btye(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d btye(s)\n", sizeof(long long int));
-	printf("Size of a float: %d btye(s)\n", sizeof(float));
+	printf("Size of a char: %s btye(s)\n", sizeof(char));
+	printf("Size of an int: %s btye(s)\n", sizeof(int));
+	printf("Size of a long int: %s btye(s)\n", sizeof(long int));
+	printf("Size of a long long int: %s btye(s)\n", sizeof(long long int));
+	printf("Size of a float: %s btye(s)\n", sizeof(float));
 	return (0);
 }
