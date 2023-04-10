8e5995adf7784131d12a524623cd92853866a4cb
Ruth-Ehornam <ehornam7@gmail.com>
2023-04-10 08:40:25 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int binary_int = 0;
-	int i;
-
 	if (b == NULL)
 		return (0);
+	unsigned int binary_int = 0;
+	int i;
 
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i]; i++)
 	{
 	if (b[i] != '0' && b[i] != '1')
 		return (0);
-	binary_int = binary_int * 2 + (b[i] - '0');
+	binary_int = (binary_int * 2) + (b[i] - '0');
 	}
 
 	return (binary_int);
