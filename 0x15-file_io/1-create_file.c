7b4e0f9870c2506e489e250907d586e7f03de061
Ruth-Ehornam <ehornam7@gmail.com>
2023-04-14 17:01:40 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,32 +7,26 @@
  *
  * Return: 1 if it success. -1 if it fails.
  */
-int create_file(const char *filename, const char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
 	if (filename == NULL)
-	return (-1);
+		return (-1);
 
 	if (text_content != NULL)
 	{
-	while (text_content[len] != '\0')
-		len++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (fd == -1)
-		return (-1);
-
 	w = write(fd, text_content, len);
 
-	if (w == -1)
-	{
-	close(fd);
+	if (fd == -1 || w == -1)
 		return (-1);
-	}
 
 	close(fd);
+
 	return (1);
 }
